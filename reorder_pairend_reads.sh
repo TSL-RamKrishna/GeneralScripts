fb0107711e9af202b83beaae3a23f619bf0abffa
Ram Krishna Shrestha <ram_krishna.shrestha@tsl.ac.uk>
2019-04-08 11:52:32 +0100

    Update reorder_pairend_reads.sh

--- reorder_pairend_reads.sh
+++ reorder_pairend_reads.sh
@@ -15,7 +15,10 @@ R2=$2
 echo "Working on Read1"
 
 cat $R1 | paste - - - - | awk '{print $1}' | sort > seqids.txt
-cat $R1 | paste - - - - > reformated_read1.txt ; split --suffix-length 5 --additional-suffix _seqids  -l 100 seqids.txt ;  split --suffix-length 5 --additional-suffix _reads -l 1000 reformated_read1.txt ; rm reformated_read1.txt; for seqid in *_seqid; do for read in *_reads; do grep -w -f $seqid --no-group-separator  $read >> reordered_R1.fastq; done; done
+cat $R1 | paste - - - - > reformated_read1.txt ; split --suffix-length 5 --additional-suffix _seqids  -l 100 seqids.txt ;  split --suffix-length 5 --additional-suffix _reads -l 1000 reformated_read1.txt ; rm reformated_read1.txt;
+if test -e reordered_R1.fastq; then rm reordered_R1.fastq; fi
+for seqid in *_seqid; do for read in *_reads; do grep -w -f $seqid --no-group-separator  $read >> reordered_R1.fastq; done; done
+cat reordered_R1.fastq |  awk '{print $1"\n"$2"\n"$3"\n"$4}' > sorted_R1.fastq; rm reordered_R1.fastq
 
 echo "Read1 completed. Now deleting temp files"
 rm seqids.txt
@@ -25,7 +28,10 @@ rm *_seqids *_reads
 echo "Working on Read2"
 
 cat $R2 | paste - - - - | awk '{print $1}' | sort > seqids.txt
-cat $R2 | paste - - - - > reformated_read2.txt ; split --suffix-length 5 --additional-suffix _seqids  -l 100 seqids.txt ;  split --suffix-length 5 --additional-suffix _reads -l 1000 reformated_read2.txt ; rm reformated_read2.txt; for seqid in *_seqid; do for read in *_reads; do grep -w -f $seqid --no-group-separator  $read >> reordered_R2.fastq; done; done
+cat $R2 | paste - - - - > reformated_read2.txt ; split --suffix-length 5 --additional-suffix _seqids  -l 100 seqids.txt ;  split --suffix-length 5 --additional-suffix _reads -l 1000 reformated_read2.txt ; rm reformated_read2.txt;
+if test -e reordered_R2.fastq; then rm reordered_R2.fastq; fi
+for seqid in *_seqid; do for read in *_reads; do grep -w -f $seqid --no-group-separator  $read >> reordered_R2.fastq; done; done
+cat reordered_R2.fastq |  awk '{print $1"\n"$2"\n"$3"\n"$4}' > sorted_R2.fastq; rm reordered_R2.fastq
 
 echo "Read2 completed. Now deleting temp files"
 rm seqids.txt
