53c364f7a7139a753800e42dc19c94865e5917a9
Ram Krishna Shrestha <ram_krishna.shrestha@tsl.ac.uk>
2019-12-11 12:56:48 +0000

    Update awk_filter_mising_snps.sh

--- awk_filter_mising_snps.sh
+++ awk_filter_mising_snps.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/env awk
+# this script filters out any snp rows from vcf file where alternative column (5th column is '.')
+# usage:
+# cat sample.vcf | awk_filter_missing_snps.sh 
 
 awk '{if($0 ~ /^#/){print} else if($5 == "."){pass} else {print}}'
