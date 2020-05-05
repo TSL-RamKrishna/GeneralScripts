994868120a9d0fa9187b381135a5c21acd16e54c
Ram Krishna Shrestha <ram_krishna.shrestha@tsl.ac.uk>
2020-05-05 11:12:20 +0100

    Update fastq_quality_check_remove_unequal_seq_qual.sh

--- fastq_quality_check_remove_unequal_seq_qual.sh
+++ fastq_quality_check_remove_unequal_seq_qual.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # usage: bash $0 fastq
+filel=$1
 if [[ "$file" == *".fq" || "$file" == *".fastq" ]]; then
-  #cat $1 | awk '{if(NR%4==1) {seqid=$0; print}; if(NR%4==2){seq=$0; print}; if(NR%4==3){print}; if(NR%4==0){qual=$0; if(length(seq)==length(qual)) {print qual} else {print "sequence and quality not same length; sequence id - " seqid > "/dev/stderr"; exit}} }'
+  #cat $file | awk '{if(NR%4==1) {seqid=$0; print}; if(NR%4==2){seq=$0; print}; if(NR%4==3){print}; if(NR%4==0){qual=$0; if(length(seq)==length(qual)) {print qual} else {print "sequence and quality not same length; sequence id - " seqid > "/dev/stderr"; exit}} }'
   cat $1 | awk '{if(NR%4==1) {seqid=$0}; if(NR%4==2){seq=$0}; if(NR%4==3){}; if(NR%4==0){qual=$0; if(length(seq)==length(qual)) {print seqid"\n"seq"\n+\n"qual} } }'
 elif [[ "$file" == *".fq.gz" || "$file" == *".fastq.gz" ]]; then
-  #zcat $1 | awk '{if(NR%4==1) {seqid=$0; print}; if(NR%4==2){seq=$0; print}; if(NR%4==3){print}; if(NR%4==0){qual=$0; if(length(seq)==length(qual)) {print qual} else {print "sequence and quality not same length; sequence id - " seqid > "/dev/stderr"; exit}} }'
-  zcat $1 | awk '{if(NR%4==1) {seqid=$0}; if(NR%4==2){seq=$0}; if(NR%4==3){}; if(NR%4==0){qual=$0; if(length(seq)==length(qual)) {print seqid"\n"seq"\n+\n"qual} } }'
+  #zcat $file | awk '{if(NR%4==1) {seqid=$0; print}; if(NR%4==2){seq=$0; print}; if(NR%4==3){print}; if(NR%4==0){qual=$0; if(length(seq)==length(qual)) {print qual} else {print "sequence and quality not same length; sequence id - " seqid > "/dev/stderr"; exit}} }'
+  zcat $file | awk '{if(NR%4==1) {seqid=$0}; if(NR%4==2){seq=$0}; if(NR%4==3){}; if(NR%4==0){qual=$0; if(length(seq)==length(qual)) {print seqid"\n"seq"\n+\n"qual} } }'
 fi
