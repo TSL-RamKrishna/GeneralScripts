be400f002398410d8649bd523a7ca98e77504848
Ram Krishna Shrestha <ram_krishna.shrestha@tsl.ac.uk>
2020-09-22 11:52:24 +0100

    Update get_subset_of_sequences_using_awk.sh

--- get_subset_of_sequences_using_awk.sh
+++ get_subset_of_sequences_using_awk.sh
@@ -2,5 +2,12 @@
 
 # awk command to get few sequences of sequences from a large fasta with many many sequences.
 # here only 10 sequences are obtained
-number=10
-awk -v number=number 'BEGIN{count=0}{if($1 ~/>/){count=count+1;seqid=$0;   if(count > number){exit} else {print seqid}}  else { print $0}}' large_sequences.fa > few_seqs.fasta
+# usage: bash $0 10 input.fasta output.fasta
+
+number=$1
+input=$2
+output=$3
+
+awk -v number=$number 'BEGIN{count=0}{if($1 ~/>/){count=count+1;seqid=$0;   if(count > number){exit} else {print seqid}}  else { print $0}}' $input > $output
+
+echo "Output to the file $output"
