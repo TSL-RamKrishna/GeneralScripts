1257ce14ef1d635d4cd848d518bc8f9e909cbcd9
tsl-ramkrishna <ram_krishna.shrestha@tsl.ac.uk>
2021-03-08 10:44:44 +0000

    update, help added

--- extract_fasta_sequence_by_id.sh
+++ extract_fasta_sequence_by_id.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+
 # a short quick program uisng awk, paste and grep command to extract fasta sequence by list of seqids  in a file
 # fasta sequences can be in mulitple lines, so here we are reading the input fasta file using the script read_fasta_with_awk.sh
 # this converts multiple line fasta sequences to single line
@@ -6,6 +8,15 @@
 # grep -f command filters out lines containing the seqids listed in the file
 # finally, the awk code at the end, stores the sequence to variable seq and then replaces the sequence by empty char giving only the seqid and then finally prints the seqid, newline and sequence
 
+
+# usage:
+# bash $0 fastafile listToExtract.txt
+
+if [[ $1 == "-h" || $1 == "--help" ]]; then
+	echo bash $0 fastafile listToExtract.txt
+	exit 0
+fi
+ 
 fasta=$1
 seqidlist=$2
 
